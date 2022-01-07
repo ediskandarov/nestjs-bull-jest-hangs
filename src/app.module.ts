591d05762188e3d69d0c5fd1baef3a59817db4a2
Eduard Iskandarov <eduard.iskandarov@yandex.ru>
2022-01-07 17:49:25 +0500

    🔧 connect bull to application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleQueueModule } from './sample-queue/sample-queue.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [SampleQueueModule],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    SampleQueueModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/sample-queue/sample-queue.module.ts
+++ src/sample-queue/sample-queue.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SampleQueueService } from './sample-queue.service';
 import { SampleQueueController } from './sample-queue.controller';
+import { BullModule } from '@nestjs/bull';
+import { AudioProcessor } from './sample-queue.processor';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'audio',
+    })
+  ],
   controllers: [SampleQueueController],
-  providers: [SampleQueueService]
+  providers: [SampleQueueService, AudioProcessor]
 })
 export class SampleQueueModule {}
